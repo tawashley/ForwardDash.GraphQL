0499760704f86385d8c7b48f729b3ec62f1d9143
Tristan Ashley <trisashley@gmail.com>
2020-02-23 21:53:48 +0000

    feat: Outline Query.weather schema and add mock resolver

--- index.ts
+++ index.ts
@@ -4,17 +4,104 @@ import { ApolloServer, gql } from 'apollo-server-express'
 const app = express();
 const port = process.env.PORT || 4000;
 
-const server = new ApolloServer({
-  typeDefs: gql`
-    type Query {
-      hello: String
+const typeDefs =  gql`
+    # Weather Schema
+
+    type WeatherLocation {
+        name: String!
+        region: String!
+        country: String!
+        lat: Float!,
+        lon: Float!,
+        tz_id: String!
+        localtime_epoch: Int!,
+        localtime: String!
     }
-  `,
-  resolvers: {
+
+    type WeatherCondition {
+        text: String!
+        iconSrc: String!
+        code: Int! #This code can be mapped from https://www.weatherapi.com/docs/conditions.json e.g. 1000 means "Sunny"
+    }
+
+    type WeatherTemperature {
+        celsius: Float!
+        fahrenheit: Float!
+    }
+
+    type WeatherWindDirection {
+        degree: Int!
+        compass: String!
+    }
+
+    type WeatherVelocity {
+        mph: Float!
+        kph: Float!
+    }
+
+    type WeatherWind {
+        mph: Float!
+        kph: Float!
+        direction: WeatherWindDirection!
+    }
+
+    type WeatherPressure {
+        millibars: Float!
+        inches: Float!
+    }
+
+    type WeatherRain {
+        millimeters: Float!
+        inches: Float!
+    }
+
+    type WeatherCurrent {
+        temperature: WeatherTemperature!
+        feelsLike: WeatherTemperature!
+        condition: WeatherCondition!
+        wind: WeatherWind!
+        gust: WeatherVelocity!
+        pressure: WeatherPressure!
+        rain: WeatherRain!
+        humidityPercentage: Int!
+        cloudCoverPercentage: Int!
+        isDaytime: Int!
+        uvIndex: Float!
+
+    }
+
+    type Weather {
+        location: WeatherLocation!
+        current: WeatherCurrent!
+    }
+
+    # Query Schema
+
+    type Query {
+        hello: String
+        weather(location: String!): Weather
+    }
+`
+
+const resolvers = {
     Query: {
       hello: () => 'Hello world!',
+      weather: (root: any, { location }: any, context: any) => {
+          return location
+      }
     },
+    Weather: {
+        current: (location: any) => {
+            return {
+                isDaytime: false
+            }
+        }
+    }
   }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 })
 
 server.applyMiddleware({
