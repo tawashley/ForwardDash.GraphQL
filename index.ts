05640ae5058003f2385a6bcd42697076c5e44ef8
Tristan Ashley <trisashley@gmail.com>
2020-02-24 16:50:05 +0000

    feat: type up current forecast response, destruct response.current, remove old 'hello world' schema

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 import fetch from 'node-fetch'
+import { CurrentWeatherResponse } from './weather-api'
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 const typeDefs =  gql`
     # Weather Schema
-
     type WeatherLocation {
         name: String!
         region: String!
@@ -77,21 +77,19 @@ const typeDefs =  gql`
     }
 
     # Query Schema
-
     type Query {
-        hello: String
         weather(location: String!): Weather
     }
 `
 
 // Weather connector
 const WeatherConnector = {
-    getCurrentForecase: async (location: string) => {
+    getCurrentForecase: async (location: string): Promise<CurrentWeatherResponse> => {
         const url = new URL('/', 'https://api.weatherapi.com',)
 
         url.pathname = 'v1/current.json'
 
-        url.searchParams.append('key', '')
+        url.searchParams.append('key', '31c6aec8b2074ce0802210520202302')
         url.searchParams.append('q', location)
 
         return fetch(url.toString()).then((response) => response.json())
@@ -102,46 +100,47 @@ const WeatherConnector = {
 const WeatherModel = {
     getCurrentForecast: async (location: string) => {
         const response = await WeatherConnector.getCurrentForecase(location)
+        const { current } = response
 
         // TODO - map condition.code to a string
         return {
             temperature: {
-                celsius: response.current.temp_c,
-                fahrenheit: response.current.temp_f
+                celsius: current.temp_c,
+                fahrenheit: current.temp_f
             },
             feelsLike: {
-                celsius: response.current.feelslike_c,
-                fahrenheit: response.current.feelslike_f
+                celsius: current.feelslike_c,
+                fahrenheit: current.feelslike_f
             },
             condition: {
-                text: response.current.condition.text,
-                iconSrc: response.current.condition.icon,
-                code: response.current.condition.code
+                text: current.condition.text,
+                iconSrc: current.condition.icon,
+                code: current.condition.code
             },
             wind: {
-                mph: response.current.wind_mph,
-                kph: response.current.wind_kph,
+                mph: current.wind_mph,
+                kph: current.wind_kph,
                 direction: {
-                  degree: response.current.wind_degree,
-                  compass: response.current.wind_dir
+                  degree: current.wind_degree,
+                  compass: current.wind_dir
                 }
             },
             gust: {
-                mph: response.current.gust_mph,
-                kph: response.current.gust_kph
+                mph: current.gust_mph,
+                kph: current.gust_kph
             },
             pressure: {
-                millibars: response.current.pressure_mb,
-                inches: response.current.pressure_in
+                millibars: current.pressure_mb,
+                inches: current.pressure_in
             },
             rain: {
-                millimeters: response.current.precip_mm,
-                inches: response.current.precip_in
+                millimeters: current.precip_mm,
+                inches: current.precip_in
             },
-            humidityPercentage: response.current.humidity,
-            cloudCoverPercentage: response.current.cloud,
-            isDaytime: response.current.is_day,
-            uvIndex: response.current.uv,
+            humidityPercentage: current.humidity,
+            cloudCoverPercentage: current.cloud,
+            isDaytime: current.is_day,
+            uvIndex: current.uv,
         }
     }
 }
@@ -152,7 +151,6 @@ const current = async (location: any, args: any, context: any) => {
 
 const resolvers = {
     Query: {
-      hello: () => 'Hello world!',
       weather: (root: any, { location }: any, context: any) => {
           return location
       }
