b6036e0c91923eac122cefb2cf76a2a0f5ff16a2
Tristan Ashley <trisashley@gmail.com>
2020-02-24 10:40:10 +0000

    feat: Add base weather model and connector, hit API

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
+import fetch from 'node-fetch'
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -83,6 +84,37 @@ const typeDefs =  gql`
     }
 `
 
+// Weather connector
+const WeatherConnector = {
+    getCurrentForecase: async (location: string) => {
+        const url = new URL('/', 'https://api.weatherapi.com',)
+
+        url.pathname = 'v1/current.json'
+
+        url.searchParams.append('key', '')
+        url.searchParams.append('q', location)
+
+        const response = await fetch(url.toString()).then((response) => response.json())
+
+        console.log({ response })
+
+        return {
+            isDaytime: false
+        }
+    }
+}
+
+// Weather model
+const WeatherModel = {
+    getCurrentForecast: async (location: string) => {
+        return WeatherConnector.getCurrentForecase(location)
+    }
+}
+
+const current = async (location: any, args: any, context: any) => {
+    return WeatherModel.getCurrentForecast(location)
+}
+
 const resolvers = {
     Query: {
       hello: () => 'Hello world!',
@@ -91,11 +123,7 @@ const resolvers = {
       }
     },
     Weather: {
-        current: (location: any) => {
-            return {
-                isDaytime: false
-            }
-        }
+        current
     }
   }
 
